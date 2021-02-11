819c1ead3e6897277cfa2a7a51809eb580dfed87
Abhinav Singh <abhinav.foss@gmail.com>
2021-02-11 20:18:25 +0530

    Test

--- app/main.py
+++ app/main.py
@@ -95,5 +95,5 @@ def hello():
      Python 3.8 (from the example template)"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=80)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", debug=True, port=80)
